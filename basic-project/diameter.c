2abff086f647fdc71e3e629ad42bc33006937835
Szabolcs Horvát <szhorvat@gmail.com>
2025-10-20 20:30:57 +0000

    chore: update for igraph 1.0.0

--- basic-project/diameter.c
+++ basic-project/diameter.c
@@ -1,23 +1,25 @@
 #include <igraph.h>
 
 int main() {
-  igraph_integer_t num_vertices = 1000;
-  igraph_integer_t num_edges = 1000;
-  igraph_real_t diameter;
-  igraph_t graph;
+    igraph_integer_t num_vertices = 1000;
+    igraph_integer_t num_edges = 1000;
+    igraph_real_t diameter;
+    igraph_t graph;
 
-  igraph_rng_seed(igraph_rng_default(), 42);
+    igraph_setup();
+    igraph_rng_seed(igraph_rng_default(), 42);
 
-  igraph_erdos_renyi_game(&graph, IGRAPH_ERDOS_RENYI_GNM,
-                          num_vertices, num_edges,
-                          IGRAPH_UNDIRECTED, IGRAPH_NO_LOOPS);
+    igraph_erdos_renyi_game_gnm(
+        &graph,
+        num_vertices, num_edges,
+        IGRAPH_UNDIRECTED, IGRAPH_SIMPLE_SW, IGRAPH_EDGE_UNLABELED);
 
-  igraph_diameter(&graph, &diameter, NULL, NULL, NULL, NULL, IGRAPH_UNDIRECTED, /* unconn= */ true);
-  printf("Diameter of a random graph with average degree %g: %g\n",
-          2.0 * igraph_ecount(&graph) / igraph_vcount(&graph),
-          (double) diameter);
+    igraph_diameter(&graph, /* weights= */ NULL, &diameter, NULL, NULL, NULL, NULL, IGRAPH_UNDIRECTED, /* unconn= */ true);
+    printf("Diameter of a random graph with average degree %g: %g\n",
+            2.0 * igraph_ecount(&graph) / igraph_vcount(&graph),
+            (double) diameter);
 
-  igraph_destroy(&graph);
+    igraph_destroy(&graph);
 
-  return 0;
+    return 0;
 }
